e9b8ae4b0de3eb0e8176b1cd085621a2bfa7ad8c
Payal Gadge <payalgadge512@gmail.com>
2026-06-29 12:37:50 +0530

    control flow

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,9 @@ import { DataBindingComponent } from './components/data-binding/data-binding.com
 import { EmployeeListComponent } from './components/employee-list/employee-list.component';
 import { StructuralDirComponent } from './components/directive/structural-dir/structural-dir.component';
 import { AttributeDirectiveComponent } from './components/directive/attribute-directive/attribute-directive.component';
+import { IfElseComponent } from './components/controlflow/if-else/if-else.component';
+import { ForComponent } from './components/controlflow/for/for.component';
+import { SwitchComponent } from './components/controlflow/switch/switch.component';
 
 export const routes: Routes = [
     {
@@ -25,5 +28,17 @@ export const routes: Routes = [
     {
         path: 'attributedirective',
         component: AttributeDirectiveComponent
+    },
+    {
+        path: "if-else",
+        component: IfElseComponent
+    },
+    {
+        path: 'for',
+        component: ForComponent
+    },
+    {
+        path: 'switch-case',
+        component: SwitchComponent
     }
 ];
--- src/app/components/controlflow/if-else/if-else.component.ts
+++ src/app/components/controlflow/if-else/if-else.component.ts
@@ -1,12 +1,37 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-if-else',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './if-else.component.html',
   styleUrl: './if-else.component.css'
 })
 export class IfElseComponent {
+    div1Visiable: boolean = true;
+  isWarnigDivVisiable: boolean = false;
+  num1: string = '';
+  num2: string = '';
+  selectedStatus: string = '';
+
+  // constructor(private deptService: DepartmentService) {
+  //   this.deptService.onRoleChange$.subscribe((res:string)=>{
+  //     debugger;
+  //   })
+  //   this.deptService.role$.subscribe((res:string)=>{
+  //     debugger;
+  //   })
+  // }
+
+  showDiv1() { 
+    this.div1Visiable = true;
+  }
+  hideDiv1() { 
+    this.div1Visiable = false;
+  }
+  toggleDiv2() { 
+    this.isWarnigDivVisiable = !this.isWarnigDivVisiable;
+  }
 
 }
